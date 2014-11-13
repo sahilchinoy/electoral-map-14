9337fbcb1a68343afa282cd4245ed9072fbb421f
Sahil Chinoy <sahil.chinoy@gmail.com>
2014-11-13 11:43:00 -0800

    Update with DC resources

--- js/data.js
+++ js/data.js
@@ -7,7 +7,7 @@ var data = {
 				"candidateName": 'Linda Maio',
                 "lastName": 'Maio',
 				"platforms": ['Improve air quality for Berkeley residents','Focus on safety and traffic issues','Address homelessness and affordable housing'],
-				"headshot": './img/maio.jpg',
+				"headshot": 'http://www.dailycal.org/assets/uploads/2014/11/maio.jpg',
                 "color": "{'background': 'rgba(179,226,205,0.5)'}"
 			},
 			{
@@ -15,7 +15,7 @@ var data = {
 				"candidateName": 'Alejandro Soto-Vigil',
                 "lastName": 'Soto-Vigil',
 				"platforms": ['Give voice to working families in council','Create new funding for affordable housing','Provide environmental leadership'],
-				"headshot": './img/soto-vigil.jpg',
+				"headshot": 'http://www.dailycal.org/assets/uploads/2014/11/sotovigil.jpg',
                 "color": "{'background': 'rgba(253,205,172,0.5)'}"
 			},
 			{
@@ -23,7 +23,7 @@ var data = {
 				"candidateName": 'Merrilee Mitchell',
                 "lastName": 'Mitchell',
 				"platforms": ['Maintain parking availability in downtown',"Go against City Council's status quo",'End City Council meetings by 10 p.m.'],
-				"headshot": './img/mitchell.jpg',
+				"headshot": 'http://www.dailycal.org/assets/uploads/2014/11/mitchell.jpg',
                 "color": "{'background': 'rgba(203,213,232,0.5)'}"
 			}
 			]
@@ -36,7 +36,7 @@ var data = {
 				"candidateName": 'Jesse Arreguin',
                 "lastName": 'Arreguin',
 				"platforms": ['Improve downtown with new retail and more parking','Expand affordable housing and raising the minimum wage','Further efforts on climate change'],
-				"headshot": './img/arreguin.jpg',
+				"headshot": 'http://www.dailycal.org/assets/uploads/2014/11/arreguin.jpg',
                 "color": "{'background': 'rgba(179,226,205,0.5)'}"
 			},
 			]
@@ -49,7 +49,7 @@ var data = {
 				"candidateName": 'Kriss Worthington',
                 "lastName": 'Worthington',
 				"platforms": ['Fill more than 50 commission vacancies with students','Implement policy changes on Telegraph Avenue to attract business','Add more street lighting and easier bus access for wheelchair users'],
-				"headshot": './img/worthington.jpg',
+				"headshot": 'http://www.dailycal.org/assets/uploads/2014/11/worthington.jpg',
                 "color": "{'background': 'rgba(179,226,205,0.5)'}"
 			},
 			{
@@ -57,7 +57,7 @@ var data = {
 				"candidateName": 'Sean Barry',
                 "lastName": 'Barry',
 				"platforms": ['Create safe and welcoming streets through working with police','Work to increase affordable housing options','Make improvements to Telegraph Avenue'],
-				"headshot": './img/barry.jpg',
+				"headshot": 'http://www.dailycal.org/assets/uploads/2014/11/barry.jpg',
                 "color": "{'background': 'rgba(253,205,172,0.5)'}"
 			}
 			]
@@ -70,7 +70,7 @@ var data = {
 				"candidateName": 'George Beier',
                 "lastName": 'Beier',
 				"platforms": ['Build affordable housing','Revive Telegraph Avenue and downtown','Bicycle/pedestrian safety and traffic calming'],
-				"headshot": './img/beier.jpg',
+				"headshot": 'http://www.dailycal.org/assets/uploads/2014/11/beier.jpg',
                 "color": "{'background': 'rgba(179,226,205,0.5)'}"
 			},
 			{
@@ -78,7 +78,7 @@ var data = {
 				"candidateName": 'Mike Alvarez Cohen',
                 "lastName": 'Cohen',
 				"platforms": ['Safer neighborhoods and mitigated traffic','Inviting commercial districts','Fiscal and environmental sustainability'],
-				"headshot": './img/cohen.jpg',
+				"headshot": 'http://www.dailycal.org/assets/uploads/2014/11/cohen.jpg',
                 "color": "{'background': 'rgba(253,205,172,0.5)'}"
 			},
 			{
@@ -94,7 +94,7 @@ var data = {
 				"candidateName": 'Lori Droste',
                 "lastName": 'Droste',
 				"platforms": ['Promote economic vitality and community investment','Ensure safe streets','Strengthen city’s partnership with schools'],
-				"headshot": './img/droste.jpg',
+				"headshot": 'http://www.dailycal.org/assets/uploads/2014/11/droste.jpg',
                 "color": "{'background': 'rgba(244,202,228,0.5)'}"
 			}
 			]
